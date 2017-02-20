4a9d3c0f02a312861ef5ed2472d6ebfb1230ee19
Nemo <me@captnemo.in>
2017-02-20 11:15:21 +0530

    Adds note about venv and python2

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,8 @@ wget --mirror https://landing.google.com/sre/book/
 mv landing.google.com/sre/book/* .
 rm -rf landing.google.com
 cd ..
+
+# Note: This does not yet create a virtual environment
+# and only runs on Python 2
 pip install -r requirements.txt
 python2 generate.py
