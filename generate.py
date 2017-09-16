9a24819d341f1fdbb73613a456e2b894d87bec3e
Nemo <me@captnemo.in>
2017-09-16 21:28:50 +0530

    Minor changes

--- generate.py
+++ generate.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
-import os, pypub
+import os
+import pypub
 
 epub = pypub.Epub('Site Reliability Engineering')
+
 
 def setup_toc():
     soup = BeautifulSoup(open('./html/index.html'), 'html.parser')
@@ -12,14 +14,20 @@ def setup_toc():
 
     epub.create_epub(os.path.abspath('./build'))
 
+
 def add_chapter_file(href, title):
     file_path = href.replace('/sre/book/', 'html/')
 
     with open(file_path, 'r') as f:
         contents = f.read()
         chapter_soup = BeautifulSoup(contents, 'html.parser')
-        chapter_html = chapter_soup.select_one('.content').prettify("utf-8")
-        chapter = pypub.create_chapter_from_string(chapter_html, url=None, title=title)
+        chapter_soup = chapter_soup.select_one('.content')
+        links = chapter_soup.select_all('a')
+        for link in links:
+            link.href = link.href.replace('/sre/book/chapters/', '')
+        chapter = pypub.create_chapter_from_string(
+            chapter_html, url=None, title=title)
         epub.add_chapter(chapter)
 
 setup_toc()
+epub.create_epub('./build')
